05a7cda881e64e1d4fb1507d9723aa76fb29a8a2
rdc201213 <44917521+rdc201213@users.noreply.github.com>
2019-09-12 20:25:58 +0800

    Add files via upload

---
+++ THE FIRST DEATH/THE FIRST DEATH/Repos/Student.cpp
@@ -0,0 +1,46 @@
+﻿#include "Student.h"
+Student::Student(int ID,
+	string Name,
+	string Sex,
+	int Age,
+	string Test)
+{
+	StudentID = ID;
+	StudentName = Name;
+	StudentSex = Sex;
+	StudentAge = Age;
+	TestName = Test;
+	Last = nullptr;
+	Next = nullptr;
+}
+
+Student::Student(Student* LastStudent,
+	int ID,
+	string Name,
+	string Sex,
+	int Age,
+	string Test)
+{
+	StudentID = ID;
+	StudentName = Name;
+	StudentSex = Sex;
+	StudentAge = Age;
+	TestName = Test;
+	Last = LastStudent;
+	Next = nullptr;
+}
+Student::~Student()
+{
+
+}
+Student* Student::Find()
+{
+}
+void Student::Change()
+{
+
+}
+void Student::Delete()
+{}
+void Student::Insert()
+{}
